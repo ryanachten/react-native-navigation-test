430f80ac25ddc7a10f3b26b4731f7e602e14a729
Ryan Achten <ryanachten@gmail.com>
2018-11-27 18:19:49 +1300

    Add init reducer structure and firebase config

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import React, { Component } from 'React';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './reducers';
 
 class App extends Component {
 
+  componentWillMount(){
+      const config = {
+        apiKey: 'AIzaSyDGBmN8fRymZYA_dkAFSsR12W9lzjc40tg',
+        authDomain: 'manager-98439.firebaseapp.com',
+        databaseURL: 'https://manager-98439.firebaseio.com',
+        projectId: 'manager-98439',
+        storageBucket: 'manager-98439.appspot.com',
+        messagingSenderId: '179190179577'
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
-      <Provider store={createStore(() => [])}>
+      <Provider store={createStore(reducers)}>
         <View>
           <Text>
             Hello
