a177402175a0a87cbae2f3bfe873be258445632a
Ryan Achten <ryanachten@gmail.com>
2018-12-04 19:11:39 +1300

    Add init EmployeeCreate form

--- src/components/EmployeeCreate.js
+++ src/components/EmployeeCreate.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'React';
 import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
   render() {
     return (
-      <View>
-        <Text>Employee Form</Text>
-      </View>
+      <Card>
+        <CardSection>
+          <Input label="Name" placeholder="Jane"/>
+        </CardSection>
+
+        <CardSection>
+          <Input label="Phone" placeholder="+64 00 000 0000"/>
+        </CardSection>
+
+        <CardSection>
+          <Button>Create</Button>
+        </CardSection>
+      </Card>
     );
   }
 }
